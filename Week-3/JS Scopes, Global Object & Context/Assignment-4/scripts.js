004c0e8beba9491af4aa3e1ec2d7df156453e4cb
Rajeev <rajeevranjan2k11@gmail.com>
2023-09-18 16:58:13 +0530

    week-3 JS scopes,Global  Assignment-4 using map

--- Week-3/JS Scopes, Global Object & Context/Assignment-4/scripts.js
+++ Week-3/JS Scopes, Global Object & Context/Assignment-4/scripts.js
@@ -1,11 +1,19 @@
 function calcWordFrequencies(){
     let prompInput = prompt()
-    const ArrStr = prompInput.split(" ")
-    for (let i = 0; i < ArrStr.length; i++) {
-        // console.log(ArrStr[i]+(i+1))
-        console.log(`${ArrStr[i]} ${i+1}`)
+    const ArrStr = prompInput.split(" ")        // convert string into Array with " " separation
+    const words = new Map();
+    for(const item of ArrStr){
+        const normalWord = item.toLowerCase();   // convert to lowercase each word
+        if(words.has(normalWord)){
+           // If the word is already in the map, increment its frequency       
+            words.set(normalWord, words.get(normalWord)+1);
+        }else{
+            words.set(normalWord,1);
+        }
     }
-    
+    words.forEach((freq,word)=>{
+        console.log(`${word} ${freq}`)
+    })
 
 }
 calcWordFrequencies();
